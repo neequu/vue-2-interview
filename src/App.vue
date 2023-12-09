472cb1ae7aa83e910d85654a8083f5b49e4da71f
neequu <nist1701@gmail.com>
2023-12-10 02:45:17 +0300

    chore: add transitions

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <script lang="ts" setup>
 import TheHeader from './components/TheHeader.vue';
+import { useRoute } from './utils/constants';
+const route = useRoute()
 </script>
 
 <template>
   <div class="page">
     <TheHeader />
     <main>
-      <router-view />
+      <transition name="slide-fade" mode="out-in">
+        <router-view :key="route.path" />
+      </transition>
     </main>
   </div>
 </template>  
 
+<style lang="scss" scoped>
+.slide-fade-enter-active {
+  transition: all 300ms ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 300ms cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
